6d04cd144b924302e1636977682ebb1af6b4b703
RobertUriarte <robbyuriarte2@gmail.com>
2021-06-04 15:34:35 -0400

    Maven

--- src/main/java/Exercise8.java
+++ src/main/java/Exercise8.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
 public class Exercise8 {
     public static void main(String[] args) { //Program to determine number of pizzas per person at pizza party
         Scanner scan = new Scanner(System.in);
-        System.out.printf("How many people do you have? ");
+        System.out.print("How many people do you have? ");
         int people = scan.nextInt();
-        System.out.printf("How many pizzas do you have? ");
+        System.out.print("How many pizzas do you have? ");
         int pizza = scan.nextInt();
-        System.out.printf("How many slices per pizza? ");
+        System.out.print("How many slices per pizza? ");
         int slices = scan.nextInt();
 
         int total_pizza = slices * pizza;
